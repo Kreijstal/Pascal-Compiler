ab294e88d1cef13ee9fb648c3a5736822cb96db4
Kreijstal <rainb@tfwno.gf>
2025-10-29 23:40:57 +0100

    Enhance Pascal const parsing and case codegen

--- GPC/CodeGenerator/Intel_x86-64/codegen_expression.h
+++ GPC/CodeGenerator/Intel_x86-64/codegen_expression.h
@@ -14,6 +14,7 @@ ListNode_t *codegen_simple_relop(struct Expression *, ListNode_t *,
     CodeGenContext *ctx, int *);
 
 ListNode_t *codegen_expr(struct Expression *, ListNode_t *, CodeGenContext *ctx);
+ListNode_t *codegen_expr_with_result(struct Expression *, ListNode_t *, CodeGenContext *ctx, Register_t **out_reg);
 ListNode_t *codegen_array_access(struct Expression *, ListNode_t *, CodeGenContext *, Register_t *);
 ListNode_t *codegen_array_element_address(struct Expression *, ListNode_t *, CodeGenContext *, Register_t **);
 ListNode_t *codegen_record_access(struct Expression *, ListNode_t *, CodeGenContext *, Register_t *);
