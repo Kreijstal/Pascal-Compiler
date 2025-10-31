2176ceb1c85468ce51bd04692c8a7921a4ff20f8
Kreijstal <rainb@tfwno.gf>
2025-11-01 00:09:01 +0100

    Handle recursive array bases across semantics and codegen

--- GPC/Parser/ParseTree/tree_types.h
+++ GPC/Parser/ParseTree/tree_types.h
@@ -48,6 +48,12 @@ struct RecordField
     int type;
     char *type_id;
     struct RecordType *nested_record;
+    int is_array;
+    int array_start;
+    int array_end;
+    int array_element_type;
+    char *array_element_type_id;
+    int array_is_open;
 };
 
 struct RecordType
@@ -305,6 +311,14 @@ struct Expression
     int pointer_subtype;
     char *pointer_subtype_id;
     struct RecordType *record_type;
+    int is_array_expr;
+    int array_element_type;
+    char *array_element_type_id;
+    int array_lower_bound;
+    int array_upper_bound;
+    int array_element_size;
+    int array_is_dynamic;
+    struct RecordType *array_element_record_type;
 };
 
 struct SetElement
